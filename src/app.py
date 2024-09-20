d8b507ec0e1a98ae1ce2226be4e2e421199aba11
IgnacioVelutini <153306785+IgnacioVelutini@users.noreply.github.com>
2024-09-20 21:58:15 +0000

    Exercise web scrapping

--- src/app.py
+++ src/app.py
@@ -6,3 +6,116 @@ import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+pip install pandas requests
+
+import requests
+import time
+
+url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+html_data = requests.get(url, time.sleep(10)).text
+
+# connect as anonymous if no info is extracted
+if "403 Forbidden" in html_data:
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
+    request = requests.get(url, headers = headers)
+    time.sleep(10)
+    html_data = request.text
+
+html_data
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(html_data,"html.parser")
+soup
+
+# Find tables
+tables = soup.find_all("table")
+tables
+
+
+import pandas as pd
+
+for index, table in enumerate(tables):
+    if ("Tesla Quarterly Revenue" in str(table)):
+        table_index = index
+        break
+
+# DataFrame
+tesla_revenue = pd.DataFrame(columns = ["Date", "Revenue"])
+for row in tables[table_index].tbody.find_all("tr"):
+    col = row.find_all("td")
+    if (col != []):
+        Date = col[0].text
+        Revenue = col[1].text.replace("$", "").replace(",", "")
+        tesla_revenue = pd.concat([tesla_revenue, pd.DataFrame({
+            "Date": Date,
+            "Revenue": Revenue
+        }, index = [0])], ignore_index = True)
+
+tesla_revenue.head()
+
+
+tesla_revenue = tesla_revenue[tesla_revenue["Revenue"] != ""]
+tesla_revenue.head()
+
+
+import sqlite3
+
+connection = sqlite3.connect("Tesla.db")
+connection
+
+
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE revenue (Date, Revenue)""")
+
+tesla_tuples = list(tesla_revenue.to_records(index = False))
+tesla_tuples[:5]
+
+
+cursor.executemany("INSERT INTO revenue VALUES (?,?)", tesla_tuples)
+connection.commit()
+
+
+for row in cursor.execute("SELECT * FROM revenue"):
+    print(row)
+
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fig, axis = plt.subplots(figsize = (10, 5))
+
+tesla_revenue["Date"] = pd.to_datetime(tesla_revenue["Date"])
+tesla_revenue["Revenue"] = tesla_revenue["Revenue"].astype('int')
+sns.lineplot(data = tesla_revenue, x = "Date", y = "Revenue")
+
+plt.tight_layout()
+
+plt.show()
+
+#Annual Gross
+fig, axis = plt.subplots(figsize = (10, 5))
+
+tesla_revenue["Date"] = pd.to_datetime(tesla_revenue["Date"])
+tesla_revenue_yearly = tesla_revenue.groupby(tesla_revenue["Date"].dt.year).sum().reset_index()
+
+sns.barplot(data = tesla_revenue_yearly[tesla_revenue_yearly["Date"] < 2023], x = "Date", y = "Revenue")
+
+plt.tight_layout()
+
+plt.show()
+
+
+#Monthly Gross
+
+fig, axis = plt.subplots(figsize = (10, 5))
+
+tesla_revenue_monthly = tesla_revenue.groupby(tesla_revenue["Date"].dt.month).sum().reset_index()
+
+sns.barplot(data = tesla_revenue_monthly, x = "Date", y = "Revenue")
+
+plt.tight_layout()
+
+plt.show()
